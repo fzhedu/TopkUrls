318d9b56b80b4541b66ff49f9069504f920e352e
fzhedu <fzhedu@gmail.com>
2019-04-19 11:04:58 +0800

    partition files ok

--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,16 @@
 #include <set>
 using namespace std;
 
-#define URL_SIZE 64
+#define URL_MAX_SIZE 64
 #define TOPK 100
 int MOD = 1e9 + 7;
 int tmp_file_num = 0;
-#define FILE_SIZE 100 * 1024 * 1024  // 100 MB
+#define FILE_SIZE 128  // 100 * 1024 * 1024  // 100 MB
 set<string> overflow_partition_file;
 
 // typedef unsigned long long uint64_t;
 string data_set_file = "data_set.txt";
+
 void MkDir(const char *dir) {
   if (access(dir, 0) == -1) {
     cout << dir << " is not existing" << endl;
@@ -50,7 +51,6 @@ void Getfilepath(const char *path, const char *filename, char *filepath) {
   strcpy(filepath, path);
   if (filepath[strlen(path) - 1] != '/') strcat(filepath, "/");
   strcat(filepath, filename);
-  //  printf("path is = %s\n", filepath);
 }
 
 bool RmFile(const char *path) {
@@ -120,14 +120,23 @@ uint64_t HashStr(string str, int seed) {
   }
   return temp;
 }
-int FileSize(const char *filename) {
+uint64_t FileSize(const char *filename) {
   struct stat info;
   stat(filename, &info);
   return info.st_size;
 }
+unsigned GetPartitionNum(const char *filename) {
+  uint64_t size = FileSize(filename);
+  return size / FILE_SIZE * 2 + 11;
+}
+// approximate size of a map
+unsigned GetMapSize(map<string, int> *mmap) {
+  return sizeof(*mmap) + mmap->size() * URL_MAX_SIZE;
+}
 /*
- * hash partition the data in the map @url_count, then write each bucket to a
- * parition file
+ * Hash partition the data in the map @url_count, then write each bucket to a
+ * partition file. Note the seed is different in each calling so that the
+ * over-sized file can be split into many smaller files.
  */
 bool WritePartition(map<string, int> *url_count, int par_num, int seed) {
   map<string, int>::iterator iter;
@@ -148,24 +157,33 @@ bool WritePartition(map<string, int> *url_count, int par_num, int seed) {
     if (bucket[par_id].size() > 0) {
       sstr.str("");
       sstr << "./tmp/partition" << (tmp_file_num + par_id);
-      if (FileSize(sstr.str().c_str()) >
-          FILE_SIZE) {  // if the partition file is too large
-        overflow_partition_file.insert(sstr.str());
-      }
+      // write to a paritition file
       ofstream fout(sstr.str(), ios::app);
       if (fout.fail()) {
-        cout << "error: create partition file failed!" << endl;
+        cout << "error: create or open partition file failed!" << endl;
         fout.close();
         return false;
       }
-      for (int j = 0; j < bucket[par_id].size(); ++j) {
-        fout << bucket[par_id][j].first << " " << bucket[par_id][j].second << endl;
+      // if the partition file is too large, then it has to be repartitioned
+      if (FileSize(sstr.str().c_str()) > FILE_SIZE) {
+        overflow_partition_file.insert(sstr.str());
+      }
+      for (unsigned j = 0; j < bucket[par_id].size(); ++j) {
+        fout << bucket[par_id][j].first << " " << bucket[par_id][j].second
+             << endl;
       }
       fout.close();
     }
   }
   return true;
 }
+/*
+ * Hash partition the raw dataset to many small partition files.
+ * First, the raw data is reduced using a map to get partial results, then the
+ * reduced data are written to partition files on the disk. Due to the
+ * limit memory, if the memory footprint of the map  exceed the limit size, i.e,
+ * FILE_SIZE, then the map should be flashed to the disk.
+ */
 bool PartitionRawData() {
   ifstream file(data_set_file.c_str());
   if (file.fail()) {
@@ -173,21 +191,20 @@ bool PartitionRawData() {
     file.close();
     return false;
   }
-  char url[URL_SIZE];
-  int map_memory_size = 0, par_num = 10, seed = tmp_file_num + 111;
+  char url[URL_MAX_SIZE];
+  int par_num = GetPartitionNum(data_set_file.c_str()),
+      seed = tmp_file_num + 111;
   map<string, int> url_count;
-  while (file.getline(url, URL_SIZE)) {
+  while (file.getline(url, URL_MAX_SIZE)) {
     url_count[url]++;
-    map_memory_size =
-        sizeof(url_count) +
-        url_count.size() * (sizeof(decltype(url_count)::key_type) +
-                            sizeof(decltype(url_count)::mapped_type));
-    if (map_memory_size > FILE_SIZE) {  // overflow
+
+    if (GetMapSize(&url_count) > FILE_SIZE) {  // exceed the limit
       WritePartition(&url_count, par_num, seed);
       url_count.clear();
     }
   }
   file.close();
+  // partition the remaining data in the map
   if (!url_count.empty()) {
     WritePartition(&url_count, par_num, seed);
     url_count.clear();
@@ -195,36 +212,39 @@ bool PartitionRawData() {
   tmp_file_num += par_num;
   return true;
 }
+/*
+ * Repartition a partition file that exceeds the limit of FILE_SIZE
+ * The main function is similar to the PartitionRawData().
+ * Note: the data format here differs that in the raw data set file
+ * data format: "url count" per line
+ */
 bool Repartition(const char *filename) {
   map<string, int> url_count;
   string url;
   int count;
-  int map_memory_size = 0, par_num = FileSize(filename) / FILE_SIZE + 3,
-      seed = tmp_file_num + 111;
+  int par_num = GetPartitionNum(filename), seed = tmp_file_num + 111;
   ifstream fin(filename, ios::in);
   if (fin.fail()) {
     fin.close();
-    cout << "error: read file failed!!" << endl;
+    cout << "error: read file failed!!" << filename << endl;
     return false;
   }
   while (fin >> url >> count) {
     if (fin.eof()) break;
     url_count[url] += count;
-    map_memory_size =
-        sizeof(url_count) +
-        url_count.size() * (sizeof(decltype(url_count)::key_type) +
-                            sizeof(decltype(url_count)::mapped_type));
-    if (map_memory_size > FILE_SIZE) {  // overflow
+    if (GetMapSize(&url_count) > FILE_SIZE) {  // exceed the limit
       WritePartition(&url_count, par_num, seed);
       url_count.clear();
     }
   }
   fin.close();
+  // partition the remaining data in the map
   if (!url_count.empty()) {
     WritePartition(&url_count, par_num, seed);
     url_count.clear();
   }
   tmp_file_num += par_num;
+  // remove the over-sized partition file
   RmFile(filename);
   return true;
 }
@@ -263,13 +283,9 @@ bool Reduce() {
     }
     fin.close();
   }
-  //  cout << "---- after read from file------" << endl;
-  //  for (iter = url_count.begin(); iter != url_count.end(); iter++) {
-  //    cout << iter->first << " -> " << iter->second << endl;
-  //  }
-  // cout << "----------------" << endl;
+#if DEBUG
   cout << "--------top " << TOPK << "----------" << endl;
-
+#endif
   if (url_count.size() <= TOPK) {
     priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> topk(
         url_count.begin(), url_count.end());
@@ -304,6 +320,7 @@ bool Reduce() {
 
 int main() {
   MkDir("./tmp");
+  assert(FILE_SIZE > 127);
   overflow_partition_file.clear();
   GenData(10);
   // partition data into small parts
@@ -313,6 +330,6 @@ int main() {
   // count top K for each partition
   Reduce();
   // sleep(10000);
-  RmFile("./tmp");
+  // RmFile("./tmp");
   return 0;
 }
